4de3e615b40ebb642e5eee3c51f3d4fc8b209051
Riccardo Palleschi <riccardo.palleschi@gmail.com>
2023-02-28 21:49:37 +0100

    add about section

--- src/commands/about.js
+++ src/commands/about.js
@@ -1,10 +1,17 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('about')
         .setDescription('Displays additional information about the project'),
     async execute(interaction) {
-        await interaction.reply({content: 'Made with ❤️ by ZigoZaro', ephemeral: true});
+        const embed = new EmbedBuilder()
+            .setColor(0xD4AF37)
+            .setTitle(`📖 jayr33n/cephalon-suaus`)
+            .setURL('https://github.com/jayr33n/cephalon-suaus')
+            .setDescription('Cephalon Suaus is a Discord bot that provides quick item lookups for Warframe.')
+            .setAuthor({name: 'jayr33n', iconURL: 'https://github.com/jayr33n.png', url: 'https://github.com/jayr33n'})
+            .setThumbnail('https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
+        await interaction.reply({embeds: [embed]});
     },
 };
